226afe447aa322b78df0caf2e39c4422be8556b4
Tae Young Lee <taeleeswe@gmail.com>
2024-05-02 09:57:46 -0400

    fixed code to list comprehension

--- main.py
+++ main.py
@@ -24,10 +24,13 @@ while len(guessed_states) < 50:
     print(answer_state)
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+
+        # changed to list comprehension
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("state_to_learn.csv")
         break
